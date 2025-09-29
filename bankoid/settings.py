711213a06ffb98196b4375789bf1f541bdeefaf5
Taiwo <adebiyitaiwosamuel009@gmail.com>
2025-09-29 08:01:17 -0700

    rectificedt commit

--- bankoid/settings.py
+++ bankoid/settings.py
@@ -136,8 +136,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
 STATIC_URL = '/static/'
 
 if not DEBUG:
